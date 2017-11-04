a01437200e8cb1eaa6eebd40d8550bbf5667097b
Luna Mendes <lkmnds@gmail.com>
2017-11-04 03:10:10 -0300

    add basic Guild callbacks

--- lib/gateway/guild.ex
+++ lib/gateway/guild.ex
@@ -1,3 +1,18 @@
+defmodule Guild do
+  use GenServer
+
+  def start() do
+    GenServer.start(__MODULE__, :ok, [name: :guilds])
+  end
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def all_guilds(user_id) do
+  end
+end
+
 defmodule GenGuild do
   use GenServer
 end
