04efd8f9e526668c8a78b63512d9cf11ddbb26ef
Luna Mendes <lkmnds@gmail.com>
2018-02-24 19:54:49 -0300

    guild: fix typo

--- lib/guild/model.ex
+++ lib/guild/model.ex
@@ -91,7 +91,7 @@ defmodule Guild do
   Get a complete guild object, given the GenGuild PID
   and the State PID.
   """
-  @spec get_complete_guild(pid(), pid()) :: Map.t
+  @spec guild_dump(pid(), pid()) :: Map.t
   def guild_dump(guild_pid, state_pid) do
     guild_id = GenGuild.get(guild_pid, :id)
     guild_map = guild_id |> Guild.get_guild |> Guild.from_struct
